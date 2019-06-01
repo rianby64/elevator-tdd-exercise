a554ca0a1cbbb4b211767e106ad82bc6cf975a39
Andres Rios <rianby64@gmail.com>
2019-06-01 19:44:16 +0300

    [TEST] Highlight the buttons in the panel via props

--- src/app/LiftPanel.test.tsx
+++ src/app/LiftPanel.test.tsx
@@ -3,13 +3,13 @@ import * as React from 'react';
 import { mount } from 'enzyme';
 import { LiftPanel } from './LiftPanel';
 
-test('LiftPanel highlighs the currentLevel', (): void => {
+test('LiftPanel highlights the currentLevel', (): void => {
     const el = (<LiftPanel currentLevel={3} floors={5} />);
     const sh = mount(el);
     expect(sh).toMatchSnapshot();
 });
 
-test('LiftPanel highlighs the pressed button', (): void => {
+test('LiftPanel highlights the pressed button', (): void => {
     const buttonPressed: number[] = [];
     const highlightButton = (i: number): () => void =>
         (): void => { buttonPressed.push(i); }
@@ -23,4 +23,14 @@ test('LiftPanel highlighs the pressed button', (): void => {
     sh.findWhere(el => el.key() === '2').find('button').simulate('click');
     expect(buttonPressed).toHaveLength(1);
     expect(buttonPressed[0]).toBe(3);
+});
+
+test('LiftPanel highlights the given set of buttons', (): void => {
+    const highlightedButtons = new Set([3, 2]);
+    const el = (<LiftPanel
+        currentLevel={3}
+        floors={5}
+        highlightedButtons={highlightedButtons}/>);
+    const sh = mount(el);
+    expect(sh).toMatchSnapshot();
 });
